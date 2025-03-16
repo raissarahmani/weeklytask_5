54cb9b4017e9a63b44de5b96fa468c74359491d5
rrhmn <raissarahmani18@gmail.com>
2025-03-16 19:00:31 +0700

    add: order history page and route

---
+++ src/components/HeaderProfile.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import Dropdown from '../assets/dropdown.png'
+import Search from '../assets/Search.png'
+import Pp from '../assets/pp.png'
+import Menu from '../assets/menu.png'
+
+function HeaderProfile() {
+  return (
+    <header className='flex flex-row items-center bg-[#fff] py-[5vh] px-[10vw] sticky top-0 z-[10]'>
+        <div className='w-3/20'>
+            <img className='w-7/10' src="../../public/tickitz2.png" alt="Logo Tickitz" />
+        </div>
+        <nav className='flex flex-row justify-evenly w-3/5'>
+            <Link to='/'>Home</Link>
+            <Link to='now-playing'>Movie</Link>
+            <Link to='/now-playing/detail'>Buy Ticket</Link>
+        </nav>
+        <div className='flex flex-row justify-right items-center w-1/4'>
+            <div>Location</div>
+            <div className='flex flex-row justify-between items-center mx-[1vw]'>
+                <img className='object-contain w-[2vw] h-[3vh]' src={Dropdown} alt=""/>
+                <img className='object-contain w-[2vw] h-[3vh]' src={Search} alt=""/>
+            </div>
+            <div className='rounded-full'><Link to='/profile-page'><img className='object-cover  h-[7vh] w-[7vh] rounded-full' src={Pp} alt="Profile"/></Link></div>
+        </div>
+        <div className='hidden'>
+            <img src={Menu} alt="Menu" />
+            <div class="hamburger-menu">
+                <nav>
+                    <p>Home</p>
+                    <p>Movie</p>
+                    <p>Buy Ticket</p>
+                    <p>Profile</p>
+                </nav>
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default HeaderProfile
--- src/pages/ProfilePage/Option.jsx
+++ src/pages/ProfilePage/Option.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Option() {
   return (
     <section className='flex flex-row rounded-2xl bg-[#fff] py-[4vh] px-[3vw]'>
-        <p className='text-lg text-[#AAAAAA] mr-[3vw] cursor-pointer'>Account Setting</p>
-        <p className='text-lg text-[#AAAAAA] mr-[3vw] cursor-pointer'>Order History</p>
+        <p className='text-lg text-[#AAAAAA] mr-[3vw] cursor-pointer'><Link to='/profile-page'>Account Setting</Link> </p>
+        <p className='text-lg text-[#AAAAAA] mr-[3vw] cursor-pointer'><Link to='/profile-page/history'>Order History</Link></p>
     </section>
   )
 }
