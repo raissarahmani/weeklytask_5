189ae0ebdc831dcce2a48866529fa9d7785e8a1e
rrhmn <raissarahmani18@gmail.com>
2025-03-20 22:45:01 +0700

    add: responsive layout

--- src/pages/Home/Hero.jsx
+++ src/pages/Home/Hero.jsx
@@ -2,26 +2,26 @@ import React from 'react'
 
 function Hero() {
   return (
-    <section className='flex flex-row px-[10vw] py-[10vh] bg-[#fff]'>
-        <div className='w-3/5 px-[1vw] py-[15vh]'>
-            <p className='font-subtitle text-[#1D4ED8]'>MOVIE TICKET PURCHASES #1 IN INDONESIA</p>
-            <p className='font-normal text-5xl/15 text-[#121212] my-[2vh]'>Experience the Magic of Cinema: Book Your Tickets Today</p>
+    <section className='flex flex-col md:flex-row px-[10vw] md:py-[10vh] bg-[#fff]'>
+        <div className='md:w-3/5 px-[1vw] md:py-[15vh] py-[10vh] text-center md:text-left'>
+            <p className='font-subtitle text-[#1D4ED8] '>MOVIE TICKET PURCHASES #1 IN INDONESIA</p>
+            <p className='font-normal md:text-5xl/15 text-4xl/12 text-[#121212] my-[2vh]'>Experience the Magic of Cinema: Book Your Tickets Today</p>
             <p className='font-normal text-[#A0A3BD]'>Sign up and get the ticket with a lot of discount</p>
         </div>
-        <div className='flex flex-row w-2/5 px-[1vw]'>
+        <div className='flex flex-row justify-center md:w-2/5 px-[1vw] my-[5vh] md:my-[0]'>
             <div className='px-[0.5vw]'>
-                <div className='w-[15vw] h-[30vh]'>
+                <div className='w-[35vw] md:w-[15vw] h-[25vh] md:h-[30vh]'>
                     <img className='object-cover object-center-top w-full h-full rounded-t-3xl py-[1vh]' src="../../public/poster6.png" alt="Movie Poster" />
                 </div>
-                <div className='w-[15vw] h-[40vh]'>
+                <div className='w-[35vw] md:w-[15vw] h-[45vh] md:h-[40vh]'>
                     <img className='object-cover object-center-top w-full h-full rounded-b-3xl py-[1vh]' src="../../public/poster1.png" alt="Movie Poster" />
                 </div>
             </div>
             <div className='px-[0.5vw]'>
-                <div className='w-[15vw] h-[40vh]'>
+                <div className='w-[35vw] md:w-[15vw] h-[45vh] md:h-[40vh]'>
                     <img className='object-cover object-center-top w-full h-full rounded-t-3xl py-[1vh]' src="../../public/poster5.png" alt="Movie Poster" />
                 </div>
-                <div className='w-[15vw] h-[30vh]'>
+                <div className='w-[35vw] md:w-[15vw] h-[25vh] md:h-[30vh]'>
                     <img className='object-cover object-center-top w-full h-full rounded-b-3xl py-[1vh]' src="../../public/poster7.png" alt="Movie Poster" />
                 </div>
             </div>
--- src/pages/Home/Intro.jsx
+++ src/pages/Home/Intro.jsx
@@ -7,29 +7,29 @@ import ResponsiveIcon from '../../assets/chat.png';
 function Intro() {
   return (
     <section className='py-[2vh] px-[10vw] bg-[#fff]'>
-        <div className='font-subtitle text-[#1D4ED8]'>WHY CHOOSE US</div>
-        <div className='font-normal text-4xl text-[#121212] my-[3vh]'>Unleashing the Ultimate Movie <br /> Experience</div>
-        <div className='flex flex-row justify-between items-center mt-[7vh]'>
-            <div className='px-[1vw]'>
-                <div className='icon-container'>
+        <div className='font-subtitle text-[#1D4ED8] text-center md:text-left'>WHY CHOOSE US</div>
+        <div className='font-normal text-4xl/12 md:text-4xl text-[#121212] my-[3vh] text-center md:text-left'>Unleashing the Ultimate Movie <br /> Experience</div>
+        <div className='flex flex-col md:flex-row md:justify-between items-center mt-[7vh]'>
+            <div className='px-[1vw] my-[5vh]'>
+                <div className='icon-container left-1/2 md:left-0 -translate-1/2 md:translate-[0]'>
                     <img className='w-1/2 h-1/2 absolute top-1/4 left-1/4' src={GuaranteedIcon} alt="Guaranteed" />
                 </div>
-                <div className='font-subtitle text-[#18181B] my-[2vh]'>Guaranteed</div>
-                <div className='font-normal text-[#A0A3BD]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, architecto similique</div>    
+                <div className='font-subtitle text-[#18181B] my-[0] md:my-[2vh] text-center md:text-left'>Guaranteed</div>
+                <div className='font-normal text-[#A0A3BD] text-center md:text-left'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, architecto similique</div>    
             </div>
-            <div className='px-[1vw]'>
-                <div className='icon-container'>
+            <div className='px-[1vw] my-[5vh]'>
+                <div className='icon-container left-1/2 md:left-0 -translate-1/2 md:translate-[0]'>
                     <img className='w-1/2 h-1/2 absolute top-1/4 left-1/4' src={AffordableIcon} alt="Affordable" />
                 </div>
-                <div className='font-subtitle text-[#18181B] my-[2vh]'>Affordable</div>
-                <div className='font-normal text-[#A0A3BD]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, architecto similique</div>    
+                <div className='font-subtitle text-[#18181B] my-[2vh] text-center md:text-left'>Affordable</div>
+                <div className='font-normal text-[#A0A3BD] text-center md:text-left'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, architecto similique</div>    
             </div>
-            <div className='px-[1vw]'>
-                <div className='icon-container'>
+            <div className='px-[1vw] my-[5vh]'>
+                <div className='icon-container left-1/2 md:left-0 -translate-1/2 md:translate-[0]'>
                     <img className='w-1/2 h-1/2 absolute top-1/4 left-1/4' src={ResponsiveIcon} alt="24/7 Customer Service" />
                 </div>
-                <div className='font-subtitle text-[#18181B] my-[2vh]'>24/7 Customer Service</div>
-                <div className='font-normal text-[#A0A3BD]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, architecto similique</div>    
+                <div className='font-subtitle text-[#18181B] my-[2vh] text-center md:text-left'>24/7 Customer Service</div>
+                <div className='font-normal text-[#A0A3BD] text-center md:text-left'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, architecto similique</div>    
             </div>
        </div>
     </section>
