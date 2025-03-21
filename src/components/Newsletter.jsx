bc2d541f10ed23a70ced600aeffdb33a8543ccfe
rrhmn <raissarahmani18@gmail.com>
2025-03-21 13:56:38 +0700

    add: responsive layout

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -5,14 +5,14 @@ import Circle from "../assets/shape.png";
 function Newsletter() {
   return (
     <section className='py-[5vh] px-[10vw]'>
-        <div className='bg-[#2563EB] rounded-3xl p-[15vh] relative'>
-            <div className='newsletter-text text-center mb-[10vh]'>Subscribe to our newsletter</div>
-            <form className='flex flex-row justify-between gap-[1vw] px-[10vw]'>
+        <div className='bg-[#2563EB] rounded-3xl py-[15vh] px-[10vw] relative'>
+            <div className='newsletter-text text-3xl md:text-5xl text-center mb-[10vh]'>Subscribe to our newsletter</div>
+            <form className='flex flex-col md:flex-row justify-between gap-[1vw] md:px-[10vw]'>
                 <label for="firstname" hidden></label>
-                <input className='newsletter-input' type="text" name="firstname" placeholder="First name" />
+                <input className='newsletter-input w-full md:w-1/3' type="text" name="firstname" placeholder="First name" />
                 <label for="email" hidden></label>
-                <input className='newsletter-input' type="email" name="email" placeholder="Email address" />
-                <button className='custom-button w-1/3 bg-[#fff] text-[#2563EB]' type="submit">Subscribe Now</button>
+                <input className='newsletter-input w-full md:w-1/3' type="email" name="email" placeholder="Email address" />
+                <button className='custom-button w-full md:w-1/3 bg-[#fff] text-[#2563EB] py-[2vh]' type="submit">Subscribe Now</button>
             </form>
             <p className='invisible font-semibold text-xs text-[#fff] my-[2vh] text-center'><span className='px-[1vw]'>Thank you!</span></p>
             <img className='absolute bottom-0 right-0' src={Circle} alt="" />
--- src/pages/Home2.jsx
+++ src/pages/Home2.jsx
@@ -9,7 +9,7 @@ function Home2() {
   return (
     <>
     <section className='bg-[50%_50%] relative z-0 before:content-[""] before:absolute before:inset-0 before:-z-1 before:bg-[#00000099]' style={{backgroundImage: `url(${Background})`}}>
-        <div className='py-[15vh] px-[10vw] w-3/5'>
+        <div className='py-[15vh] px-[10vw] md:w-3/5'>
             <p className='font-subtitle text-[#fff]'>LIST MOVIE OF THE WEEK</p>
             <p className='font-medium text-4xl text-[#fff] my-[2vh]'>Experience the Magic of Cinema: Book Your Tickets Today</p>
         </div>
